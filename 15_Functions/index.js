259c055bb9caf267bd4f742315d86f08abfc4204
jordan <jharrison1@qa.com>
2022-05-26 14:51:32 +0100

    added functions example

---
+++ 15_Functions/index.js
@@ -0,0 +1,24 @@
+"use strict" // tells the browser to hold you to modern JS standards
+multiply(20, 2);
+
+function hoisting() {
+    console.log(x);
+    x = 5;
+    console.log(x);
+    var x  = 2;
+    console.log(x);
+}
+
+function noHoisting() {
+    console.log(x);
+    x = 5;
+    console.log(x);
+    let x  = 2;
+    console.log(x);
+}
+
+
+function impliedGlobal() {
+    j = 27;
+    console.log(j);
+}
